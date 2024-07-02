3884bc2e3651e123229df8b9e7618d4cd8cde94d
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-07-02 17:22:58 +0300

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,24 @@ export function Header() {
         <header>
             <section>
                 <nav>
-
+                    <div className="header_nav__container">
+                        <a href="#"></a>
+                        <div className="header_shopping_cart__header_user__container">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <img src="../../src/media/header/spopping_cart.svg" alt="spopping_cart" />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="../../src/media/header/user_default_avatar.png" alt="user_default_avatar" />
+                                    </a>
+                                    <span className="user_name">Coder</span>
+                                </li>
+                            </ul>
+                        </div>  
+                    </div>
                 </nav>
             </section>
         </header>
