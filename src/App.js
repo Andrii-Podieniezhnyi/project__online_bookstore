9a0549f7a41eff08dc486e9339b358499c192caa
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-07-15 16:06:24 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import {Header} from './components/header/header';
 import './components/header/header.css'
 import './App.css';
