5ccb2de4c2c002f817175b9f5ece3c6380b6c390
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-07-15 16:10:09 +0300

    update

--- src/App.js
+++ src/App.js
@@ -10,28 +10,24 @@ function App() {
       <div className="App">
         <Header />
       </div>
-      <div className="container">
-        <h1 className="text-center">Hello, Bootstrap!</h1>
-        <button className="btn btn-primary">Click me</button>
-        <div className='container mt-4'>
-          <div className='row'>
-            <div className='col-lg-3 col-md-4 col-sm-6 mb-4'>
-              <div className="card">
+      <div className='container mt-4'>
+        <div className='row'>
+          <div className='col-lg-3 col-md-4 col-sm-6 mb-4'>
+            <div className="card">
+              <img src="path/to/book1.jpg" className="card-img-top" alt="Book 1" />
+              <div className="card-body">
+                <h5 className="card-title">Book Title 1</h5>
+                <p className="card-text">Author Name</p>
+              </div>
+            </div>
+          </div>
+          <div className='col-lg-3 col-md-4 col-sm-6 mb-4'>
+            <div className="card">
                 <img src="path/to/book1.jpg" className="card-img-top" alt="Book 1" />
                 <div className="card-body">
                   <h5 className="card-title">Book Title 1</h5>
                   <p className="card-text">Author Name</p>
                 </div>
-              </div>
-            </div>
-            <div className='col-lg-3 col-md-4 col-sm-6 mb-4'>
-              <div className="card">
-                  <img src="path/to/book1.jpg" className="card-img-top" alt="Book 1" />
-                  <div className="card-body">
-                    <h5 className="card-title">Book Title 1</h5>
-                    <p className="card-text">Author Name</p>
-                  </div>
-              </div>
             </div>
           </div>
         </div>
