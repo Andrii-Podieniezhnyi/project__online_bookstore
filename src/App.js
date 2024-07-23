0999c3fc8f2838eb8c86feacb413664ddc818bcf
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-07-23 13:23:07 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import {Header} from './components/header/header';
 import './components/header/header.css'
 import { Footer } from './components/footer/footer';
@@ -10,28 +9,6 @@ function App() {
     <div>
       <div>
         <Header />
-      </div>
-      <div className='container mt-4 main_container'>
-        <div className='row'>
-          <div className='col-lg-3 col-md-4 col-sm-6 mb-4'>
-            <div className="card">
-              <img src="path/to/book1.jpg" className="card-img-top" alt="Book 1" />
-              <div className="card-body">
-                <h5 className="card-title">Book Title 1</h5>
-                <p className="card-text">Author Name</p>
-              </div>
-            </div>
-          </div>
-          <div className='col-lg-3 col-md-4 col-sm-6 mb-4'>
-            <div className="card">
-                <img src="path/to/book1.jpg" className="card-img-top" alt="Book 1" />
-                <div className="card-body">
-                  <h5 className="card-title">Book Title 1</h5>
-                  <p className="card-text">Author Name</p>
-                </div>
-            </div>
-          </div>
-        </div>
       </div>
       <div>
         <Footer />
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 
 export function Main () {
     return(
-        <section>
-            <div>
-                
-            </div>
-        </section>
+        <main>
+            <section>
+                <div className='container mt-4 main_container'>
+                    <div className='row'>
+                      <div className='col-lg-3 col-md-4 col-sm-6 mb-4'>
+                        <div className="card">
+                          <img src="path/to/book1.jpg" className="card-img-top" alt="Book 1" />
+                          <div className="card-body">
+                            <h5 className="card-title">Book Title 1</h5>
+                            <p className="card-text">Author Name</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='col-lg-3 col-md-4 col-sm-6 mb-4'>
+                        <div className="card">
+                            <img src="path/to/book1.jpg" className="card-img-top" alt="Book 1" />
+                            <div className="card-body">
+                              <h5 className="card-title">Book Title 1</h5>
+                              <p className="card-text">Author Name</p>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     )
 }
