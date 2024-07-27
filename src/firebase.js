286304ca59d5734cf0c2d12a5c36635382a56fd3
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-07-27 16:41:19 +0300

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue} from "firebase/database"
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBHXwoWe9wSjgeY4HpG4R_TTweEhL4AXms",
+    authDomain: "script-store-50560.firebaseapp.com",
+    databaseURL: "https://script-store-50560-default-rtdb.firebaseio.com",
+    projectId: "script-store-50560",
+    storageBucket: "script-store-50560.appspot.com",
+    messagingSenderId: "126924904241",
+    appId: "1:126924904241:web:ed03652cba589ccc998210"
+  };
+
+
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+
+  export { database, ref, onValue};
