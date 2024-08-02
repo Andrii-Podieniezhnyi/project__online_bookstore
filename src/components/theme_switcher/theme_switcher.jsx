4f86c0a00dc9d3058a0e0b1783bbe27af8bab473
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-08-02 19:47:20 +0300

    update

--- src/components/theme_switcher/theme_switcher.jsx
+++ src/components/theme_switcher/theme_switcher.jsx
@@ -4,7 +4,7 @@ export function ThemeSwitcher() {
     const [theme, setTheme] = useState(() => {
         const savedTheme = localStorage.getItem('theme');
 
-        return savedTheme ? savedTheme : 'light';
+        return savedTheme ? savedTheme : 'light_theme';
     })
 
 
@@ -14,28 +14,20 @@ export function ThemeSwitcher() {
     }, [theme]);
 
     const toggleTheme = () => {
-        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
+        setTheme((prevTheme) => (prevTheme === 'light_theme' ? 'dark_theme' : 'light_theme'))
     }
 
 
     return (
         
-/*
-        <div class="color-theme">
-            <div class="color-theme__button color-theme__button--light">Light</div>
-                <div class="color-theme__indication">
-                    <div class="color-theme__indication_ico"></div>
-                </div>
-            <div class="color-theme__button color-theme__button--dark">Dark</div>
-        </div>
-*/
+
 
         <div className="color_theme_switcher" onClick={toggleTheme}>
-            <div class="color_theme_switcher__button_light">Light</div>
-                <div class="color_theme_switcher__indication">
-                    <div class="color_theme_switcher__indication_ico"></div>
+            <div className="color_theme_switcher__button_light">Light</div>
+                <div className="color_theme_switcher__indication">
+                    <div className="color_theme_switcher__indication_ico"></div>
                 </div>
-            <div class="color_theme_switcher__button_dark">Dark</div>
+            <div className="color_theme_switcher__button_dark">Dark</div>
         </div>
     )
 }
