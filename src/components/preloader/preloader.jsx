d796bbeef13232ed3bc4e77a60e35ba85dc71464
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-08-03 20:11:08 +0300

    update

---
+++ src/components/preloader/preloader.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import './preloader.css'
+
+
+export function Preloader() {
+    return(
+        <div class="tetris">
+		    <div class="tetris__block tetris__block--1">
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    </div>
+		    <div class="tetris__block tetris__block--2">
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    </div>
+		    <div class="tetris__block tetris__block--3">
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    </div>
+		    <div class="tetris__block tetris__block--4">
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    </div>
+		    <div class="tetris__block tetris__block--5">
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    </div>
+		    <div class="tetris__block tetris__block--6">
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    </div>
+		    <div class="tetris__block tetris__block--7">
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    </div>
+		    <div class="tetris__block tetris__block--8">
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    </div>
+		    <div class="tetris__block tetris__block--9">
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    </div>
+		    <div class="tetris__block tetris__block--10">
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    	<div class="tetris__block-cell"></div>
+		    </div>
+	    </div>
+    )
+}
