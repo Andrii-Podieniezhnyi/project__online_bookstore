efbd3bc9788a3f28c0fba959a918ec1c5cfb4334
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-08-06 18:42:10 +0300

    update

--- src/components/book_context/book_provider.jsx
+++ src/components/book_context/book_provider.jsx
@@ -5,3 +5,28 @@ import { BookContext } from "./book_context";
 
 
 
+export const BookProvider = ({ children }) => {
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const booksRef = ref(database, 'books');
+        onValue(booksRef, (snapshot) => {
+            const data = snapshot.val();
+      
+            console.log("Data from Firebase:", data); 
+      
+            if (data) {
+              const booksArray = Object.values(data);
+              setBooks(booksArray);
+            } else {
+              console.log("No data available");
+            }
+            setLoading(false);
+          }, (error) => {
+            console.error("Error fetching data:", error);
+            console.log(error.message);
+            setLoading(false);
+          });
+    })
+}
