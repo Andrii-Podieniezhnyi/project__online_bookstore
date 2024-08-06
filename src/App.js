d9b90db7268a3a26d177fed1d780b644e1cd47df
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-08-06 19:00:31 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routs} from 'react-router-dom';
+import { BookProvider } from './components/book_context/book_provider';
 import { Header } from './components/header/header';
 import './components/header/header.css'
 import { Footer } from './components/footer/footer';
 import './components/footer/footer.css';
 import BookList from './components/book_list/book_list';
+
 import './components/book_list/book_list.css';
 import './style/index.css'
 
---
+++ src/components/book_modal/book_modal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function BookModal () {
+    return (
+        console.log('Modal window is active')
+    )
+}
