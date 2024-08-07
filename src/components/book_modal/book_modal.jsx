eb57ea0f7a99c8dc0964ceea94a07b8066fe8ff1
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-08-07 20:39:46 +0300

    update

--- src/components/book_modal/book_modal.jsx
+++ src/components/book_modal/book_modal.jsx
@@ -7,24 +7,10 @@ export const BookModal = ({ book, onClose }) => {
     if(!book) return null;
 
 
-  
-
-    
-
-    const closeButtonStyles = {
-        position: 'absolute',
-        top: '10px',
-        right: '10px',
-        border: 'none',
-        background: 'none',
-        fontSize: '20px',
-        cursor: 'pointer',
-    };
-
     return (
         <div className="modal_window_style">
             <div className="modal_window__content_style">
-                <button style={closeButtonStyles} onClick={onClose}>&times;</button>
+                <button className="modal_window__close_window_btn" onClick={onClose}>&times;</button>
                 <h2>{book.title}</h2>
                 <p>{book.description}</p>
                 <ul>
