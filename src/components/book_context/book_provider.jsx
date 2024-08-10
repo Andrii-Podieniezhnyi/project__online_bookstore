0e37c19c006d50b722d211f8f6bf83e678e47071
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2024-08-10 19:29:10 +0300

    update

--- src/components/book_context/book_provider.jsx
+++ src/components/book_context/book_provider.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { ref, onValue } from 'firebase/database';
 import { database } from "../../firebase";
 import { BookContext } from "./book_context";
+const [selectedLevel, setSelectedLevel] = useState('all');
 
 
 
