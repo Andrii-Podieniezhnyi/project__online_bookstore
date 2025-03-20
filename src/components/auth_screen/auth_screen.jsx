1634073c4e65faec967321cf62e01a3b976169dc
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-03-20 21:36:57 +0200

    update

--- src/components/auth_screen/auth_screen.jsx
+++ src/components/auth_screen/auth_screen.jsx
@@ -1,2 +1,13 @@
 import React from "react";
 import './auth_screen.css';
+
+const AuthScreen = () => {
+
+    return (
+        <main>
+            <section>
+                <div className="hi">HI</div>
+            </section>
+        </main>
+    )
+}
