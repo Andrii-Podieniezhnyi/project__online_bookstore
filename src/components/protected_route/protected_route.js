549904613e38ae5711d9942bb2c00ea9ed459c84
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-03-23 20:55:38 +0200

    update

--- src/components/protected_route/protected_route.js
+++ src/components/protected_route/protected_route.js
@@ -2,3 +2,9 @@ import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../auth_screen/auth_context";
 
+export const ProtectedRoute = ({children}) => {
+    const {user} = useContext(AuthContext);
+
+    return user ? children : <Navigate to = "/auth" />;
+}
+
