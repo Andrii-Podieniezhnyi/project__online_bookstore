c0fabb9648894ba31500c43d2d9419b4e8b01aee
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-03-23 20:43:33 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { BookProvider } from './components/book_context/book_provider';
 import { Header } from './components/header/header';
-import './components/header/header.css'
 import { Footer } from './components/footer/footer';
-import './components/footer/footer.css';
 import BookList from './components/book_list/book_list';
 import { BookModal } from './components/book_modal/book_modal';
+import { AuthScreen } from './components/auth_screen/auth_screen';
+import { AuthProvider } from './components/auth_screen/auth_context';
+import { Navigate } from 'react-router-dom';
+
+
+import './components/header/header.css'
+import './components/footer/footer.css';
 import './components/book_list/book_list.css';
 import './style/index.css'
 import './components/book_modal/book_modal.css'
-import { AuthScreen } from './components/auth_screen/auth_screen';
 import './components/auth_screen/auth_screen.css'
-import { AuthProvider } from './components/auth_screen/auth_context';
-import { Navigate } from 'react-router-dom';
+
+
 
 
 function App() {
