11085513422a833780a96d1cc96ef5a1f9fe6ee5
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-03-26 20:47:47 +0200

    update

--- src/components/auth_screen/auth_context.js
+++ src/components/auth_screen/auth_context.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState} from "react";
+import { auth, loginWithGoogle, logout } from "../../firebase";
 
 export const AuthContext = createContext();
 
@@ -6,10 +7,19 @@ export const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
 
-    const login = (userData) => {
-        setUser(userData);
+
+    // Вхід через Google
+
+    const handlelogin = async () => {
+        const userData = await loginWithGoogle();
+
+        if(userData){
+            setUser(userData);
+        }
     };
 
+
+    // 
     const logout = () => {
         setUser(null);
     }
