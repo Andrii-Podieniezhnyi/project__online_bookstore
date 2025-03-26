63bb92e36136c99f26c72f5941ccec736d06f676
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-03-26 21:20:38 +0200

    update

--- src/components/auth_screen/auth_screen.jsx
+++ src/components/auth_screen/auth_screen.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useContext} from "react";
+import { AuthContext } from "./auth_context";
 import './auth_screen.css';
 
 export const AuthScreen = () => {
+    const { loginWithGoogle } = useContext(AuthContext)
+
 
     return (
         <main>
             <section>
-                <div className="hi">HI</div>
+                <h2>Ласкаво просимо</h2>
+                <button className="google-login-btn" onClick={loginWithGoogle}>
+                    Увійте через Google
+                </button>
             </section>
         </main>
     )
