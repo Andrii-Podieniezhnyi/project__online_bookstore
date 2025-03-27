f6ac7d685cfc1f3b7a3c26263f939623415b976d
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-03-27 21:15:43 +0200

    update

--- src/components/auth_screen/auth_screen.jsx
+++ src/components/auth_screen/auth_screen.jsx
@@ -1,17 +1,20 @@
 import React, { useContext } from "react";
 import { AuthContext } from "./auth_context";
-import "./auth_screen.css"; // Додамо туди стилі
+import "./auth_screen.css";
 
 export const AuthScreen = () => {
     const { login } = useContext(AuthContext);
 
     return (
         <div className="auth-screen">
-            <h2>Script Store</h2>
-            <button className="google-btn" onClick={login}>
-                <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google" className="google-icon" />
-                Увійти через Google
-            </button>
+            <div className="wave"></div>
+            <div className="auth-card">
+                <h2>Script Store</h2>
+                <button className="google-btn" onClick={login}>
+                    <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google" className="google-icon" />
+                    Увійти через Google
+                </button>
+            </div>
         </div>
     );
 }
