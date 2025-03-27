d33c0872bd251c74208f4592746c0f5c027d56e1
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-03-27 21:02:39 +0200

    update

--- src/components/auth_screen/auth_context.js
+++ src/components/auth_screen/auth_context.js
@@ -29,7 +29,7 @@ export const AuthProvider = ({ children }) => {
 
 
     return (
-        <AuthContext.Provider value={{ user, login: handleLogin, logout: handleLogout, logout}}>
+        <AuthContext.Provider value={{ user, login: handleLogin, logout: handleLogout}}>
             {children}
         </AuthContext.Provider>
     )
