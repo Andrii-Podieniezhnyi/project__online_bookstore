2d160ed9d40606eaf0aad6e2bbc8aaca70cdb8ac
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-04-01 21:26:52 +0300

    update

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,3 +2,18 @@ import React from "react";
 import { useLocation, Outlet } from "react-router-dom";
 import { Header } from "../header/header";
 import { Footer } from "../footer/footer";
+
+
+export const Layout =  () => {
+    const location = useLocation();
+    const isAuthPage = location.pathname === "/auth";
+
+
+    return(
+        <>
+            {!isAuthPage && <Header />}
+                <Outlet />
+            {!isAuthPage && <Footer />}
+        </>
+    )
+}
