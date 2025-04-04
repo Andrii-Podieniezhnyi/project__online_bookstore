c0b01ffd6c46e68ccdcf38c3dfc1b0dc66955c76
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-04-04 22:48:28 +0300

    update

--- src/components/auth_screen/auth_screen.jsx
+++ src/components/auth_screen/auth_screen.jsx
@@ -1,29 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { AuthContext } from "./auth_context";
 import { useNavigate } from "react-router-dom";
 import "./auth_screen.css";
 
 export const AuthScreen = () => {
-    const { login } = useContext(AuthContext);
+    const { login, user } = useContext(AuthContext);
     const navigate = useNavigate();
 
-    const handleLogin = async () => {
-        const user = await login();
-        if(user) {
+    // Перевіряємо, чи є користувач після логіну
+    useEffect(() => {
+        if (user) {
             navigate("/booklist");
         }
-    }
+    }, [user, navigate]); // Оновлення коли user змінюється
 
     return (
         <div className="auth-screen">
             <div className="wave"></div>
             <div className="auth-card">
                 <h2>Script Store</h2>
-                <button className="google-btn" onClick={handleLogin}>
-                    <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google" className="google-icon" />
+                <button className="google-btn" onClick={login}>
+                    <img
+                        src="https://www.svgrepo.com/show/475656/google-color.svg"
+                        alt="Google"
+                        className="google-icon"
+                    />
                     Увійти через Google
                 </button>
             </div>
         </div>
     );
-}
+};
