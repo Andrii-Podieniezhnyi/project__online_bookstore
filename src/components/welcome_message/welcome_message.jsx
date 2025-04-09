c5b059731acc4397c2808243717fb58a0a9f3e62
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-04-09 21:20:08 +0300

    update

--- src/components/welcome_message/welcome_message.jsx
+++ src/components/welcome_message/welcome_message.jsx
@@ -1,3 +1,6 @@
 import React, { useContext} from "react";
 import { AuthContext } from "../auth_screen/auth_context";
- 
+
+export const WelcomeMessage = () => {
+    const {user} = useContext(AuthContext)
+}
