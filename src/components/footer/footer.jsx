9a25d01fa7297a6e79fcf404346235ffb24b6b0a
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-04-15 20:51:51 +0300

    update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,6 +6,7 @@ export function Footer() {
             <section>
                 <div className="footer__container">
                     <span className="footer__content">Script Store © 2024</span>
+                    <p>{new Date().getFullYear}</p>
                 </div>
             </section>
         </footer>
