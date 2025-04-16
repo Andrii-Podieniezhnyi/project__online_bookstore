e2e903c2be6a649fcb0a58a98f777f8bfde2aedf
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-04-16 20:38:26 +0300

    update

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ import './components/auth_screen/auth_screen.css'
 function App() {
 
   return (
-    <Router>
+    <Router basename='/project__online_bookstore'>
       <AuthProvider>
         <BookProvider>
           <Routes>
