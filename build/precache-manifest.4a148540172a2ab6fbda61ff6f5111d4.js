fbc694beb3a52ca9e76c0bb961a93537fe650584
Andrii-Podieniezhnyi <lincolnap23@gmail.com>
2025-04-18 21:09:13 +0300

    "Збережено зміни та додано .gitignore"

---
+++ build/precache-manifest.4a148540172a2ab6fbda61ff6f5111d4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2c7bba15f8b9d602e45d3cc8975e00",
+    "url": "/project__online_bookstore/index.html"
+  },
+  {
+    "revision": "84b6460a66859607324a",
+    "url": "/project__online_bookstore/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "0056ef09ed6890d1cb9d",
+    "url": "/project__online_bookstore/static/css/main.1987e94a.chunk.css"
+  },
+  {
+    "revision": "84b6460a66859607324a",
+    "url": "/project__online_bookstore/static/js/2.139be551.chunk.js"
+  },
+  {
+    "revision": "0056ef09ed6890d1cb9d",
+    "url": "/project__online_bookstore/static/js/main.58766aa5.chunk.js"
+  },
+  {
+    "revision": "c30b0798af1f96ae67bf",
+    "url": "/project__online_bookstore/static/js/runtime~main.fcd37b47.js"
+  },
+  {
+    "revision": "ec0607e311143f2370d8eb9c2507ac7c",
+    "url": "/project__online_bookstore/static/media/pichu_ico.ec0607e3.webp"
+  },
+  {
+    "revision": "d3ada596cf5bcd856fb25bb172c39c1d",
+    "url": "/project__online_bookstore/static/media/pikachu_ico.d3ada596.png"
+  },
+  {
+    "revision": "3e0a576373329b4b072f98d34caa220e",
+    "url": "/project__online_bookstore/static/media/pikachu_ninja_ico.3e0a5763.png"
+  },
+  {
+    "revision": "c2a5a3af1f80c432edec89a96b9184a4",
+    "url": "/project__online_bookstore/static/media/pokeBall_all.c2a5a3af.png"
+  },
+  {
+    "revision": "1a03bcf3c2a4bcd6ccadf8bb866320dd",
+    "url": "/project__online_bookstore/static/media/raichu_ico.1a03bcf3.webp"
+  }
+]);
